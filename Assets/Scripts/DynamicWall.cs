70cfc3c550ea47fb5d87fbe6f5af08b943fde49a
HuygheHenri-Thibault <henrithibault.huyghe@gmail.com>
2021-04-09 21:07:07 +0200

    Dynamic Wall Added
    Need to fix speed still

---
+++ Assets/Scripts/DynamicWall.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicWall : MonoBehaviour
+{
+    public float Distance = 10.0f;
+    //public float MaxSpeed = 10.0f; //Speed is being a bitch, need to increase frequency of the wave without changing the amplitude
+    public bool IsMovingVertically = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 translation = new Vector3();
+        float distanceToMove = Mathf.Cos(Time.realtimeSinceStartup) * Distance * Time.deltaTime; // Todo: Improve
+
+        if (IsMovingVertically)
+        {
+            translation.y = distanceToMove;
+        }
+        else
+        {
+            translation.x = distanceToMove;
+        }
+
+        transform.Translate(translation);
+    }
+}
