e515517ee6cccb81af5cb57b420232d5ebc3dc9a
HuygheHenri-Thibault <henrithibault.huyghe@gmail.com>
2021-04-10 12:16:15 +0200

    Player color

--- Assets/Scripts/PlayerColour.cs
+++ Assets/Scripts/PlayerColour.cs
@@ -1,16 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerColour : MonoBehaviour
 {
     [ColorUsage(true, true)]
-    public Color TeamColour;
+    public Color MyTeamColor;
+
+    [ColorUsage(true, true)]
+    public Color TeamOneColor;
+    [ColorUsage(true, true)]
+    public Color TeamTwoColor;
+
+    private MeshRenderer _bodyRenderer;
+    private MeshRenderer _armRenderer;
+
+    private PlayerInputManager _playerInputManager;
 
     // Start is called before the first frame update
     void Start()
     {
-        //TeamColour = transform.GetChild(0).GetComponent<MeshRenderer>().material.color;
+        _playerInputManager = GameObject.Find("PlayerInputManager").GetComponent<PlayerInputManager>();
+
+        _bodyRenderer = transform.GetChild(0).GetComponent<MeshRenderer>();
+        _armRenderer = _bodyRenderer.transform.GetChild(0).GetComponent<MeshRenderer>();
+
+        if (_playerInputManager.playerCount % 2 == 0)
+        {
+            MyTeamColor = TeamOneColor;
+        }
+        else
+        {
+            MyTeamColor = TeamTwoColor;
+        }
+
+        SetInstanceColorToTeamColor();
     }
 
     //// Update is called once per frame
@@ -22,8 +47,22 @@ public class PlayerColour : MonoBehaviour
     {
         if(other.gameObject.tag == "Ball")
         {
-            other.gameObject.GetComponent<MeshRenderer>().sharedMaterial.color = TeamColour;
-            other.gameObject.GetComponent<MeshRenderer>().sharedMaterial.SetColor("_EmissionColor", TeamColour);
+            ChangeBallColorToTeamColor(other.gameObject);
         }
+    }
+
+    private void SetInstanceColorToTeamColor()
+    {
+        _bodyRenderer.material.color = MyTeamColor;
+        _bodyRenderer.material.SetColor("_EmissionColor", MyTeamColor);
+
+        _armRenderer.material.color = MyTeamColor;
+        _armRenderer.material.SetColor("_EmissionColor", MyTeamColor);
+    }
+
+    private void ChangeBallColorToTeamColor(GameObject ball)
+    {
+        ball.GetComponent<MeshRenderer>().sharedMaterial.color = MyTeamColor;
+        ball.GetComponent<MeshRenderer>().sharedMaterial.SetColor("_EmissionColor", MyTeamColor);
     }
 }
