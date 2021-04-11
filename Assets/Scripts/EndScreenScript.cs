2416ff401ac3f5a7fb13e4d6ef4263fb030d3ffd
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-04-11 13:26:17 +0200

    Endscreen partly done

---
+++ Assets/Scripts/EndScreenScript.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndScreenScript : MonoBehaviour
+{
+    [SerializeField]
+    Sprite _TeamOneWonTexture;
+    [SerializeField]
+    Sprite _TeamTwoWonTexture;
+    [SerializeField]
+    Text _scoreRed;
+    [SerializeField]
+    Text _scoreBlue;
+
+    Image _currentImage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject canvas = GameObject.Find("Canvas");
+        _currentImage = canvas.transform.GetChild(0).GetComponent<Image>();
+
+        _scoreRed = canvas.transform.GetChild(1).GetComponent<Text>();
+        _scoreBlue = canvas.transform.GetChild(2).GetComponent<Text>();
+
+        if (ScoreTracker.TeamOneScore > ScoreTracker.TeamTwoScore)
+        {
+            _currentImage.sprite = _TeamOneWonTexture;
+
+            var temp = _scoreRed.transform;
+            _scoreRed.transform.position = _scoreBlue.transform.position;
+            _scoreBlue.transform.position = temp.position;
+        }
+        else if (ScoreTracker.TeamOneScore < ScoreTracker.TeamTwoScore)
+        {
+            _currentImage.sprite = _TeamTwoWonTexture;
+        }
+        else
+        {
+            _currentImage.sprite = _TeamOneWonTexture;
+
+            var temp = _scoreRed.transform.position;
+            _scoreRed.transform.position = _scoreBlue.transform.position;
+            _scoreBlue.transform.position = temp;
+        }
+
+        _scoreBlue.text = ScoreTracker.TeamOneScore.ToString();
+        _scoreRed.text = ScoreTracker.TeamTwoScore.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
